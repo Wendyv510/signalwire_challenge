208aa227ade6699752293c738d5e2d73eb52fcf2
Wendy Harris <wendyv510@hotmail.com>
2020-09-24 13:35:51 -0400

    database attributes

--- db/migrate/20200924172154_tickets.rb
+++ db/migrate/20200924172154_tickets.rb
@@ -1,4 +1,10 @@
 class Tickets < ActiveRecord::Migration[6.0]
   def change
+    create_table :tickets do |t|
+      t.integer :user_id 
+      t.string :title 
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20200924172208_tags.rb
+++ db/migrate/20200924172208_tags.rb
@@ -1,4 +1,8 @@
 class Tags < ActiveRecord::Migration[6.0]
   def change
+    create_table :tags do |t|
+      t.array :tags 
+
+    end
   end
 end
